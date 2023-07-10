e94e370adb8f3fd024321ff0e78fbe717f449b23
Mariia Nikitash <nikitash.mariia2004@csu.fullerton.edu>
2023-07-10 16:27:57 -0700

    imports

---
+++ main.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import random
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
+
+import tensorflow_datasets as tfds
