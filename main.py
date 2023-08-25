5e9e9ab46add784fac1b7e5710d04c72b2464f99
Mariia Nikitash <nikitash.mariia2004@csu.fullerton.edu>
2023-08-24 21:43:40 -0700

    citation added

--- main.py
+++ main.py
@@ -3,6 +3,22 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import random
 import tensorflow as tf
+
 from tensorflow.keras import datasets, layers, models
 
 import tensorflow_datasets as tfds
+
+
+
+@inproceedings{bossard14,
+  title = {Food-101 -- Mining Discriminative Components with Random Forests},
+  author = {Bossard, Lukas and Guillaumin, Matthieu and Van Gool, Luc},
+  booktitle = {European Conference on Computer Vision},
+  year = {2014}
+}
+
+
+# TensorFlow has the food 101 dataset already. 
+# We will use this! https://www.tensorflow.org/datasets/catalog/food101
+
+# Hot dog is label 55
